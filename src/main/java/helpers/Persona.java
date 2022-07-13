ca0f834de81776158204975c39aecb5f986cb1f8
OscarMauricio88 <108775435+OscarMauricio88@users.noreply.github.com>
2022-07-13 17:54:45 -0600

    Creando metodos estblecer y obtener en la clase persona

--- src/main/java/helpers/Persona.java
+++ src/main/java/helpers/Persona.java
@@ -9,5 +9,53 @@ package helpers;
  * @author Oscar Mauricio
  */
 public class Persona {
+    //Atributos
+   private String nombre;
+   private String apellido;
+   private String domicilio;
+   private int dni;
+    public Persona ()
+    {
+        //Constructor sin parametros
+    }
     
+    //Establecer y obtener
+    public void establecerNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+    
+    public String obtenerNombre()
+    {
+        return this.nombre;
+    }
+    
+     public void establecerApellido(String apellido)
+    {
+        this.apellido = apellido;
+    }
+    
+    public String obtenerApellido()
+    {
+        return this.apellido;
+    }
+     public void establecerDomicilio(String domicilio)
+    {
+        this.domicilio = domicilio;
+    }
+    
+    public String obtenerDomicilio()
+    {
+        return this.domicilio;
+    }
+    
+     public void establecerDni(int dni)
+    {
+        this.dni = dni;
+    }
+    
+    public int obtenerDni()
+    {
+        return this.dni;
+    }
 }
