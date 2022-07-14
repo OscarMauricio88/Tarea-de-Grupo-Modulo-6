21b52e8e59e7ceb37e4638a6b98de420806657f8
OscarMauricio88 <108775435+OscarMauricio88@users.noreply.github.com>
2022-07-13 18:15:10 -0600

    Herendando en la subclase Doctor

---
+++ src/main/java/helpers/Doctor.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package helpers;
+
+/**
+ *
+ * @author Oscar Mauricio
+ */
+public class Doctor extends Persona {
+    
+    public Doctor()
+       { 
+           establecerProfesion("Doctor");
+           establecerNombre("Julio");
+           establecerApellido("Carranza");
+           establecerDomicilio("Tegucigalpa, Col.bendeck");
+           establecerDni(1801199345);
+           
+           
+           establecerProfesion("Doctor");
+           establecerNombre("Cesae");
+           establecerApellido("Zavala");
+           establecerDomicilio("Elprogreso, Col.Berlin");
+           establecerDni(1801199345);
+       }    
+}
