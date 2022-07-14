5f8291ac8de37e22cee3fcf92ade37166e2affd6
OscarMauricio88 <108775435+OscarMauricio88@users.noreply.github.com>
2022-07-13 18:18:41 -0600

    Herendando en la subclase Deportista

---
+++ src/main/java/helpers/Deportista.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package helpers;
+
+/**
+ *
+ * @author Oscar Mauricio
+ */
+public class Deportista extends Persona {
+    
+    public Deportista()
+            {
+           establecerProfesion("Deportista");
+           establecerNombre("Mario");
+           establecerApellido("Maldonado");
+           establecerDomicilio("Tegucigalpa, Col.bendeck");
+           establecerDni(1802349345);
+           
+           establecerProfesion("Deportista");
+           establecerNombre("Susana");
+           establecerApellido("Fuentes");
+           establecerDomicilio("La Paz, Col.barandillo");
+           establecerDni(1322349345);
+            }
+
+}
